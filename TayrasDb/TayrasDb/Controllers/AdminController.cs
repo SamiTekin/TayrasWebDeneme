16d72a632244626b6247dfe509f2ae57c9ce7691
Sami Tekin <109964125+SamiTekin@users.noreply.github.com>
2022-07-25 15:56:31 +0300

    Initial commit

---
+++ TayrasDb/TayrasDb/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TayrasDb.Models;
+
+namespace TayrasDb.Controllers
+{
+  
+    public class AdminController : Controller
+    {
+        TayrasDbEntities db = new TayrasDbEntities();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var sorgu = db.Kullanicilars.ToList();
+            return View(sorgu);
+        }
+    }
+}
